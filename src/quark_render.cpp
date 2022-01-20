ae7d2edfe43b80803af6faccb8f6a13047f03ea1
Sean Moulton <bananamoulton@gmail.com>
2022-01-20 17:44:49 -0600

    Change which face is backface because i messed something up somewhere maybe

--- src/quark_render.cpp
+++ src/quark_render.cpp
@@ -454,7 +454,7 @@ void quark::internal::init_pipelines() {
     rasterization_info.polygonMode = VK_POLYGON_MODE_FILL;
     rasterization_info.lineWidth = 1.0f;
     rasterization_info.cullMode = VK_CULL_MODE_BACK_BIT;
-    rasterization_info.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+    rasterization_info.frontFace = VK_FRONT_FACE_CLOCKWISE;
     rasterization_info.depthBiasConstantFactor = 0.0f;
     rasterization_info.depthBiasClamp = 0.0f;
     rasterization_info.depthBiasSlopeFactor = 0.0f;
