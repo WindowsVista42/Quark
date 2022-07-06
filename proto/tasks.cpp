326c247d6a25ba4bf8cddf038f5bda64ad508dac
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2022-07-06 14:42:30 -0500

    0.68s threadpool

--- proto/tasks.cpp
+++ proto/tasks.cpp
@@ -3,6 +3,9 @@
 #include <atomic>
 #include <thread>
 #include <chrono>
+#include <threadpoolapiset.h>
+#include <vector>
+#include "concurrentqueue.h"
 
 #include <iostream>
 using usize = std::uintptr_t;
@@ -12,11 +15,12 @@ atomic_usize num(0);
 atomic_usize inc(0);
 
 void do_thing() {
-  if (inc.fetch_add(1) % 2 == 0) {
-    num.fetch_add(1);
-  } else {
-    num.fetch_sub(1);
-  }
+  return;
+  //if (inc.fetch_add(1) % 2 == 0) {
+    //num.fetch_add(1);
+  //} else {
+  //  num.fetch_sub(1);
+  //}
 }
 
 struct mutex;
@@ -277,34 +281,159 @@ DWORD WINAPI incrementally_print_nums(void* data) {
   return 0;
 }
 
+template <typename T>
+struct atomic_queue {
+  atomic_usize head;
+  atomic_usize tail;
+  T arr[64] = {};
+
+  bool pop(T* t) {
+    *t = arr[head.fetch_add(1)];
+    if(*t == 0) {
+      head.fetch_sub(1);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  void push(T t) {
+    arr[tail.fetch_add(1)] = t;
+  }
+};
+
+//static atomic_queue<void (*)()> work_queue;
+static moodycamel::ConcurrentQueue<void (*)()> work_queue(100);
+static atomic_usize working_count;
+static semaphore begin_sem;
+static semaphore done_sem;
+
+static std::atomic_bool begin_lock(false);
+static std::atomic_bool done_lock(false);
+static std::atomic_bool exit_threads(false);
+atomic_usize wait_count(0);
+
+usize a = 0;
+
+DWORD WINAPI worker_main2(PVOID data) {
+  usize id = *(usize*)data;
+
+  while(true) {
+    //wait_count.fetch_add(1, std::memory_order_relaxed);
+    //begin_sem.lock();
+    //while(!begin_lock.load()) {
+    //  wait_count.fetch_or(1 << id);
+    //  begin_sem.sleep();
+    //}
+    //begin_sem.unlock();
+    //wait_count.fetch_sub(1, std::memory_order_relaxed);
+    //wait_count.fetch_and(~(1 << id));
+    wait_count.fetch_add(1, std::memory_order_relaxed);
+    begin_sem.lock();
+    begin_sem.sleep();
+    begin_sem.unlock();
+    wait_count.fetch_sub(1, std::memory_order_relaxed);
+
+    if(exit_threads.load(std::memory_order_relaxed)) {
+      printf("Thread Exiting!\n");
+      break;
+    }
+
+    void (*proc)();
+    if(work_queue.try_dequeue(proc)) {
+      working_count.fetch_add(1, std::memory_order_relaxed);
+      proc();
+
+      while(work_queue.try_dequeue(proc)) {
+        proc();
+      }
+      working_count.fetch_sub(1, std::memory_order_relaxed);
+    }
+
+    if(work_queue.size_approx() == 0 && working_count.load() == 0) {
+      while(done_lock.load(std::memory_order_relaxed) != true) {
+        done_sem.wake_all();
+      }
+    }
+  }
+
+  return 0;
+}
+
+DWORD WINAPI print(PVOID data) {
+  while(true) {
+    printf("size: %llu\n", work_queue.size_approx());
+    printf("num: %llu\n", num.load());
+    printf("working: %llu\n", working_count.load());
+    Sleep(100);
+  }
+}
+
 int main() {
-  DWORD id;
-  HANDLE h = CreateThread(0, 0, incrementally_print_nums, 0, 0, &id);
+  //DWORD id2;
+  //HANDLE h = CreateThread(0, 0, incrementally_print_nums, 0, 0, &id2);
+
+  // .init()
+  begin_sem = semaphore::create();
+  done_sem = semaphore::create();
 
   pool.init(std::thread::hardware_concurrency());
+  std::vector<HANDLE> threads;
+  std::vector<usize> ids;
+
+  for(int i = 0; i < 16; i += 1) {
+    DWORD id;
+    ids.push_back(i);
+    threads.push_back(CreateThread(0, 0, worker_main2, ids.data() + i, 0, &id));
+  }
 
   auto t0 = std::chrono::high_resolution_clock::now();
   for(int i = 0; i < 100000; i += 1) {
-    pool.add_work_begin_now(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work_begin_now(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.add_work(do_thing);
-    pool.join();
+    // .add_work()
+    for(int i = 0; i < 16; i += 1) {
+      work_queue.enqueue(do_thing);
+    }
+
+    // .join()
+    //while(wait_count.load(std::memory_order_relaxed) != 0b1111111111111111) {}
+    while(wait_count.load(std::memory_order_relaxed) != 16) {}
+    //begin_lock.store(true);
+    begin_sem.wake_all();
+    done_lock.store(false);
+    while(work_queue.size_approx() != 0 || working_count.load() != 0) {
+      done_sem.lock();
+      done_sem.sleep();
+      done_sem.unlock();
+    }
+    done_lock.store(true);
+    //begin_lock.store(false);
   }
   auto t1 = std::chrono::high_resolution_clock::now();
   std::cout << std::chrono::duration<double>(t1 - t0).count() << "s\n";
+
+  while(wait_count.load(std::memory_order_relaxed) != 16) {}
+  exit_threads.store(true);
+  begin_sem.wake_all();
+
+  WaitForMultipleObjects(threads.size(), threads.data(), TRUE, INFINITE);
+  printf("exited thread pool!\n");
+
+  //Sleep(100);
+  //SetEvent(event);
+
+  t0 = std::chrono::high_resolution_clock::now();
+  for(int i = 0; i < 100000; i += 1) {
+    for (int i = 0; i < 16; i += 1) {
+      pool.add_work(do_thing);
+    }
+    pool.join();
+  }
+  t1 = std::chrono::high_resolution_clock::now();
+  std::cout << std::chrono::duration<double>(t1 - t0).count() << "s\n";
+
+  for(int i = 0; i < 16; i += 1) {
+    TerminateThread(pool.threads[i], 0);
+  }
 
   printf("Exited num: %llu!\n", num.load());
 }
